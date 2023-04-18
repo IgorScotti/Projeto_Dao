a1d05e8321ce9df22bdbc642d4b2cd1031f822ec
IgorScotti <igorscoti@hotmail.com>
2023-04-18 19:42:26 -0300

    Criação do metodo findyByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -9,13 +9,16 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {	
-
-		
+	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+
+		Department department = new Department(2, null);
 		
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for (Seller obj: list) {
+			System.out.println(obj);
+		}
 	}
 
 }
