e3098da56490785b2c1b88b65d1ce970c8027c4a
IgorScotti <igorscoti@hotmail.com>
2023-04-20 23:06:22 -0300

    Inclusão do insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,15 +11,22 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		SellerDao sellerDao = DaoFactory.createSellerDao();
-
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();	
+		Department department = new Department(2, null);
 		
 		List<Seller> list = sellerDao.findAll();
 		
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
+		System.out.println("-----------------------------Teste Insert-----------------------------------");
+		
+		Seller newSeller = new Seller(null, "Igor", "igorscoti@hotmail.com", new Date(),4000.0 , department);
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserida! new id = " + newSeller.getId());
+		
 	}
 
 }
